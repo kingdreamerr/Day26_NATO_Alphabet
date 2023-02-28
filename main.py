8505384e0d9c0e5356f26f7a145c0fdb8cfce9fa
kingdreamerr <jackmvula458@gmail.com>
2023-02-28 13:51:26 +0200

    Print the nato code

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ data = pandas.read_csv('./nato_phonetic_alphabet.csv')
 
 nato_alphabet = {row.letter:row.code for (key,row) in data.iterrows() }
 
-word = input("Enter word: ").upper()
+word = input("Enter word to convert to NATO code: ").upper()
 
 nato_code = [nato_alphabet[letter] for letter in word]
+
+print(nato_code)
