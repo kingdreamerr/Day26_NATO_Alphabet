1d4c4130b365e08ebb4fe7b9cff2fae11da3daf0
kingdreamerr <jackmvula458@gmail.com>
2023-02-28 13:48:28 +0200

    Convert word to nato_code

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ data = pandas.read_csv('./nato_phonetic_alphabet.csv')
 
 nato_alphabet = {row.letter:row.code for (key,row) in data.iterrows() }
 
-word = input("Enter word: ")
+word = input("Enter word: ").upper()
 
+nato_code = [nato_alphabet[letter] for letter in word]
