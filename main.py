adf2a6e5af94c9a7fd9250a5812ff16a2c2c23b2
kingdreamerr <jackmvula458@gmail.com>
2023-02-28 13:46:51 +0200

    Get user daa

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ import pandas
 data = pandas.read_csv('./nato_phonetic_alphabet.csv')
 
 nato_alphabet = {row.letter:row.code for (key,row) in data.iterrows() }
-print(nato_alphabet)
+
+word = input("Enter word: ")
+
